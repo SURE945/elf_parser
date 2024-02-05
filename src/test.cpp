f90f06fcbf2839814e61e73958097c12416491a3
shuo.liu <648672722@qq.com>
2024-02-05 16:42:04 +0800

    optimize char print

--- src/test.cpp
+++ src/test.cpp
@@ -7,8 +7,9 @@ void elf_parser_test(std::fstream &fin) {
         int cnt_bytes = fin.gcount();
         for (int i = 0; i < 100; i+=4) {
             for (int j = 0; j < 4; j++) {
+                unsigned char c = buff[i + j];
                 std::cout << std::hex << std::setfill('0') 
-                << std::setw(2) << (int)(buff[i + j]) << " ";
+                << std::setw(2) << (unsigned int)(c) << " ";
             }
             std::cout << std::endl;
         }
